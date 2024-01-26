910301ec66c31e46a2262e8755d24743a3e9503f
Juan Azocar <ju.azocar@profesor.duoc.cl>
2024-01-26 20:08:38 -0300

    mensaje

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('crear-usuario', views.crear, name='crear-usuario'),
 
    ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
     context= {}
     return render(request, 'home/index.html', context)
+
+
+def crear(request):
+
+    if request.method == "POST":
+        username=request.POST["username"]
+        password=request.POST["password"]
+        email=request.POST["email"]
+        first_name=request.POST["first_name"]
+        last_name=request.POST["last_name"] 
+
+        user = User.objects.create_user(username, email, password)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
+        context= {"user": user}
+        return render(request, 'home/crear-usuario.html', context)
+    else:
+        context= {}
+        return render(request, 'home/crear-usuario.html', context)
